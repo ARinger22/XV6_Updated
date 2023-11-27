b0a77908cfa793f4928a90ae6c049e17324d4d17
Aryaman Gupta <96819393+AGlade7@users.noreply.github.com>
2023-11-27 17:10:10 +0530

    Update proc.h

--- proc.h
+++ proc.h
@@ -49,7 +49,14 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int ctime;                   // Process creation time 
+  int stime;                   // Process SLEEP time     
+  int retime;                  // Process READY time
+  int rutime;                  // Process RUN time 
+
 };
+
+void changestatistics();     // Changing Process time information
 
 // Process memory is laid out contiguously, low addresses first:
 //   text
