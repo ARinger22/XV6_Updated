ce3369ccfc25574a264e1007f3beef1b44867c41
Aryaman Gupta <96819393+AGlade7@users.noreply.github.com>
2023-11-27 17:12:14 +0530

    Update sysproc.c

--- sysproc.c
+++ sysproc.c
@@ -101,3 +101,19 @@ int sys_cps(void)
 {
   return cps();
 }
+
+int sys_wait2(void) {
+  int res;
+  int stime = 0;
+  int retime = 0;
+  int rutime = 0;
+  struct proc *curproc = myproc();
+  argint(0, &retime);
+  argint(1, &rutime);
+  argint(2, &stime);
+  res = wait();
+  *(int*)stime = curproc->stime;
+  *(int*)retime = curproc->retime;
+  *(int*)rutime = curproc->rutime;
+  return res;
+}
